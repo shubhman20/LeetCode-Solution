142eaa68f512940387576816a22edf75c3eb0f2b
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2022-12-14 22:53:35 +0530

    Create house_robber.cpp

---
+++ house_robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),0);
+        dp[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int pick = nums[i];
+            if(i>1){
+                pick+=dp[i-2];
+            }
+            int notpick = dp[i-1];
+            dp[i]=max(pick,notpick);
+        }
+        return dp[nums.size()-1];
+    }
+
+};
