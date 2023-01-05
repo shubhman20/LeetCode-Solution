6f57a36c711ba11fba424fc304c5005a5566ee3e
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2023-01-05 10:46:55 +0530

    Create minimum_number_of_arrow_to_burst.cpp

---
+++ minimum_number_of_arrow_to_burst.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+static bool comp(vector<int>&a,vector<int>&b){
+    return a[1]<b[1];
+}
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if(points.size()==1) return 1;
+        sort(points.begin(),points.end(),comp);
+        int arrow=1;
+        int end = points[0][1];
+        for(int i=0;i<points.size();i++){
+            if(points[i][0]>end){
+                arrow++;
+                end=points[i][1];
+            }
+        }
+        return arrow;
+    }
+};
