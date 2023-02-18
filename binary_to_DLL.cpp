86a971d40f323a7b9aca426046f08086de46b004
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2023-02-18 22:24:36 +0530

    GS

---
+++ binary_to_DLL.cpp
@@ -0,0 +1,38 @@
+/* Structure for tree and linked list
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+// This function should return head to the DLL
+class Solution
+{
+    public: 
+    Node* prev=NULL;
+    //Function to convert binary tree to doubly linked list and return it.
+    Node * bToDLL(Node *root)
+    {
+        // your code here
+        if(root==NULL) return root;
+        Node*head = bToDLL(root->left);
+        if(prev==NULL){
+            head=root;
+        }
+        else{
+            root->left=prev;
+            prev->right=root;
+        }
+        prev=root;
+        bToDLL(root->right);
+        return head;
+    }
+};
