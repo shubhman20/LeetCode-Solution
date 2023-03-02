b1f81d941a093d7330c64e99795719b225278399
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2023-03-02 22:19:22 +0530

    Update string_compression.cpp

--- string_compression.cpp
+++ string_compression.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
-    int f(int val){
-        while(val>=10){
-            val=val/10;
-        }
-        return val;
-    }
     int compress(vector<char>& chars) {
-        // sort(chars.begin(),chars.end());
-        string s="";
-        s+=chars[0];
-        int cnt=1;
-        // char curr,prev;
-        for(int i=1;i<chars.size();i++){
-            if(chars[i]==chars[i-1])
+        int i=0,j=0;
+        int n = chars.size();
+        if(n==1) return 1;
+        
+        while(i<n){
+            int cnt=1;
+            while(i<n-1&&chars[i]==chars[i+1]){
+                i++;
                 cnt++;
-            else{
-                if(cnt==1){
-                    s = s+chars[i];
+            }
+            chars[j++]=chars[i++];
+            if(cnt>1){
+                string s = to_string(cnt);
+                for(int k=0;k<s.size();k++){
+                    chars[j++]=s[k];
                 }
-                else{
-                    s = s+ to_string(cnt);
-                    cnt=1;
-                    s+=chars[i];
-                }
+                cnt=1;
             }
         }
-        if(cnt>1){
-            s= s + to_string(cnt);
-        }
-        vector<char> t;
-        for(int i=0;i<s.size();i++){
-            t.push_back(s[i]);
-        }
-        chars = t;
-
-        return chars.size();
+        return j;
     }
 };
