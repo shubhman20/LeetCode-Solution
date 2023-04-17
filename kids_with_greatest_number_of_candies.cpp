34d457abaf28cf86ae4452b23192c3450d6aca65
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2023-04-17 22:32:47 +0530

    Create kids_with_greatest_number_of_candies.cpp

---
+++ kids_with_greatest_number_of_candies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int n = candies.size();
+        vector<bool> ans(n,false);
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(maxi<candies[i]){
+                maxi=candies[i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(candies[i]+extraCandies>=maxi){
+                ans[i]=true;
+            }
+        }
+        return ans;
+    }
+};
